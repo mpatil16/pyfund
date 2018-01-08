279e98d87a67679fd6ae5f3d26a05f0e5a4080c7
Patil, Mahesh <mahesh.patil@pioneer.com>
2018-01-08 13:29:11 +0530

    Added nested_list.py

--- pyfund/hackerrank/basic_data_types/nested_list.py
+++ pyfund/hackerrank/basic_data_types/nested_list.py
@@ -1,1 +1,10 @@
-asd
+if __name__ == '__main__':
+	list1 = []
+	inner_list = []
+	marksheet = []
+	for _ in range(int(input())):
+		marksheet.append([input(), float(input())])
+	second_lowest = sorted(list(set([marks for name, marks in marksheet])))[1]
+	for name, marks in sorted(marksheet):
+		if marks == second_lowest:
+			print(name)
